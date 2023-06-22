61e02e2e0ec53af5b6843a131a6b4fc76512ab4b
Abigail-Mensah <117354744+Abigail-Mensah@users.noreply.github.com>
2023-06-22 23:22:29 +0000

    js files modified

--- main.js
+++ main.js
@@ -132,7 +132,7 @@ function submitForm(event) {
 
 
  
-     if (isNaN(monthValue) || monthValue < 1 || monthValue > 31) {
+     if (isNaN(monthValue) || monthValue < 1 || monthValue > 12git ) {
       monthYearError.innerText = "Invalid month";
       monthInput.style.borderColor = "red";
       isValid = false;
