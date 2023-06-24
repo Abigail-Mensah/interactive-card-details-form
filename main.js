4528d2f6eff7db7e7082aa95faafd4ca6bf50b81
Abigail-Mensah <117354744+Abigail-Mensah@users.noreply.github.com>
2023-06-24 22:35:13 +0000

    css files, html and js files modified and other functionalities added  to detect card type and cvc type

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-
+  
 var nameInput = document.getElementById("nameInput");
 var numberInput = document.getElementById("numberInput");
 var monthInput = document.getElementById("monthInput");
 var yearInput = document.getElementById("yearInput");
+// var cardNumberInput = document.getElementById("cardNumberInput");
 var cvcInput = document.getElementById("cvcInput");
+var cardTypeNumber = document.getElementById("cardTypeNumber");
+var cardTypeCVC = document.getElementById("cardTypeCVC");
 
 var numberText = document.getElementById("numberText");
 var nameText = document.getElementById("nameText");
@@ -15,12 +18,7 @@ var numberError = document.getElementById("numberError");
 var monthYearError = document.getElementById("monthYearError");
 var cvcError = document.getElementById("cvcError");
 
-numberInput.addEventListener("input", function() {
-  addSpaces();
-  numberText.innerText = numberInput.value;
-  numberError.innerText = "";
-  numberInput.style.borderColor = "";
-});
+
 
 
 
@@ -45,6 +43,66 @@ function addSpaces() {
 
 
 
+// Function to determine the card type based on the entered card number
+function getCardType(cardNumber) {
+  var cardType = "Unknown";
+
+  // Visa
+  var visaRegex = /^4[0-9]{12}(?:[0-9]{3})?$/;
+  if (visaRegex.test(cardNumber)) {
+    cardType = "Visa";
+  }
+
+  // Mastercard
+  var mastercardRegex = /^5[1-5][0-9]{14}$/;
+  if (mastercardRegex.test(cardNumber)) {
+    cardType = "Mastercard";
+  }
+
+  // American Express
+  var amexRegex = /^3[47][0-9]{13}$/;
+  if (amexRegex.test(cardNumber)) {
+    cardType = "American Express";
+  }
+
+  // Discover
+  var discoverRegex = /^6(?:011|5[0-9]{2})[0-9]{12}$/;
+  if (discoverRegex.test(cardNumber)) {
+    cardType = "Discover";
+  }
+
+  // JCB
+  var jcbRegex = /^(?:2131|1800|35\d{3})\d{11}$/;
+  if (jcbRegex.test(cardNumber)) {
+    cardType = "JCB";
+  }
+
+  // Diners Club
+  var dinersClubRegex = /^3(?:0[0-5]|[68][0-9])[0-9]{11}$/;
+  if (dinersClubRegex.test(cardNumber)) {
+    cardType = "Diners Club";
+  }
+
+  return cardType;
+}
+
+// Function to update the card type number input based on the entered card number
+function updateCardTypeNumber() {
+  var cardNumber = numberInput.value.replace(/\s/g, ''); // Remove whitespace from the card number
+  var cardType = getCardType(cardNumber);
+
+  // Update cardTypeNumber
+  cardTypeNumber.value = cardType;
+}
+
+numberInput.addEventListener("input", function() {
+  addSpaces();
+  updateCardTypeNumber();
+  numberText.innerText = numberInput.value;
+  numberError.innerText = "";
+  numberInput.style.borderColor = "";
+});
+
 
 nameInput.addEventListener("input", function() {
   var enteredName = nameInput.value;
@@ -56,11 +114,75 @@ nameInput.addEventListener("input", function() {
   nameInput.style.borderColor = "";
 });
 
-monthInput.addEventListener("input", function() {
-  updateMonthYearText();
-  monthYearError.innerText = "";
-  monthInput.style.borderColor = "";
-});
+function validateMonth(enteredMonth) {
+  return new Promise(function(resolve, reject) {
+    // Check if the entered month is within the valid range (1 to 12)
+    if (isNaN(enteredMonth) || enteredMonth < 1 || enteredMonth > 12) {
+      reject("Invalid month");
+    } else {
+      // Remove leading zero if present
+      enteredMonth = enteredMonth.replace(/^0+/, "");
+
+      // Add leading zero if the entered month is a single digit
+      if (enteredMonth.length === 1 && enteredMonth !== "0") {
+        enteredMonth = "0" + enteredMonth;
+      }
+
+      resolve(enteredMonth);
+    }
+  });
+}
+
+var timeoutId;
+
+function validateMonth(enteredMonth) {
+  return new Promise(function(resolve, reject) {
+    // Check if the entered month is within the valid range (1 to 12)
+    if (isNaN(enteredMonth) || enteredMonth < 1 || enteredMonth > 12) {
+      reject("Invalid month");
+    } else {
+      // Remove leading zero if present
+      enteredMonth = enteredMonth.replace(/^0+/, "");
+
+      resolve(enteredMonth);
+    }
+  });
+}
+
+function handleMonthInput() {
+  var enteredMonth = monthInput.value.trim();
+
+  clearTimeout(timeoutId); // Clear the previous timeout
+
+  timeoutId = setTimeout(function() {
+    validateMonth(enteredMonth)
+      .then(function(validatedMonth) {
+        monthInput.style.borderColor = "";
+        monthYearError.innerText = "";
+        monthInput.value = validatedMonth;
+
+        if (enteredMonth.length === 1 && validatedMonth.length === 1) {
+          monthInput.value = "0" + validatedMonth;
+        }
+
+        updateMonthYearText();
+      })
+      .catch(function(error) {
+        monthInput.style.borderColor = "red";
+        monthYearError.innerText = error;
+      });
+  }, 500); // Delay before performing validation
+}
+
+monthInput.addEventListener("input", handleMonthInput);
+
+
+
+
+
+
+
+
 
 yearInput.addEventListener("input", function() {
   updateMonthYearText();
@@ -68,11 +190,64 @@ yearInput.addEventListener("input", function() {
   yearInput.style.borderColor = "";
 });
 
+// cvcInput.addEventListener("input", function() {
+//   cvcRectangle.innerText = cvcInput.value;
+//   cvcError.innerText = "";
+//   cvcInput.style.borderColor = "";
+// });
+
+
+
+
+
+// Function to determine the CVC type based on the entered CVC
+function getCVCType(cvc) {
+  var cvcType = "Unknown";
+
+  // Visa CVC
+  var visaCvcPattern = /^[0-9]{3}$/;
+  if (visaCvcPattern.test(cvc)) {
+    cvcType = "Visa";
+  }
+
+  // Mastercard CVC
+  var mastercardCvcPattern = /^[0-9]{3}$/;
+  if (mastercardCvcPattern.test(cvc)) {
+    cvcType = "Mastercard";
+  }
+
+  // American Express CVC
+  var amexCvcPattern = /^[0-9]{4}$/;
+  if (amexCvcPattern.test(cvc)) {
+    cvcType = "American Express";
+  }
+
+  return cvcType;
+}
+
+
+
+// Function to update the card type CVC input based on the entered CVC
+function updateCardTypeCVC() {
+  var cvc = cvcInput.value;
+  var cvcType = getCVCType(cvc);
+  cardTypeCVC.value = cvcType;
+}
+
+
+
+
+// Event listener for the cvcInput
 cvcInput.addEventListener("input", function() {
   cvcRectangle.innerText = cvcInput.value;
   cvcError.innerText = "";
   cvcInput.style.borderColor = "";
+  updateCardTypeCVC();
 });
+
+
+
+
 
 function updateMonthYearText() {
   var month = monthInput.value;
@@ -171,7 +346,8 @@ function submitForm(event) {
     console.log("CVC: " + cvc);
 
 
-
+    // Hide inputfields
+    
     nameInput.style.display = "none";
     numberInput.style.display = "none";
     monthInput.style.display = "none";
@@ -181,6 +357,9 @@ function submitForm(event) {
     numberError.style.display="none";
     cvcError.style.display="none";
     monthYearError.style.display="none";
+    document.querySelector(".card_cvctype").style.display = "none";
+    document.getElementById("cardTypeNumber").style.display = "none";
+    document.getElementById("cardTypeCVC").style.display = "none";
 
 
 
@@ -193,6 +372,8 @@ function submitForm(event) {
   document.querySelector(".cvctext").style.display = "none";
   document.querySelector(".monthtext").style.display = "none";
   document.querySelector(".yeartext").style.display = "none";
+  document.querySelector(".cvctype_text").style.display = "none";
+  document.querySelector(".cardtype_text").style.display = "none";
 
 
     popup.style.display = "block";
@@ -247,14 +428,21 @@ function continueClicked(event) {
   numberError.style.display = "block";
   cvcError.style.display = "block";
   monthYearError.style.display = "block";
+  document.querySelector(".card_cvctype").style.display = "block";
+  document.getElementById("cardTypeNumber").style.display = "block";
+  document.getElementById("cardTypeCVC").style.display = "block";
+
+
   document.getElementById('submit').style.display = "block"
   
-  // Show the labels
+  // Show the labels again
   document.querySelector(".nametext").style.display = "block";
   document.querySelector(".numbertext").style.display = "block";
   document.querySelector(".cvctext").style.display = "block";
   document.querySelector(".monthtext").style.display = "block";
   document.querySelector(".yeartext").style.display = "block";
+  document.querySelector(".cvctype_text").style.display = "block";
+  document.querySelector(".cardtype_text").style.display = "block";
   
   // Reset the form after submission
   document.getElementById("cardForm").reset();
